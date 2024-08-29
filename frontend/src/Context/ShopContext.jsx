f22eac99db4dafdbecb8dd2690410a03cb7fe031
Tokelo Thulo <gelukthulo@gmail.com>
2024-08-29 13:24:46 +0200

    Cartpage ready

--- frontend/src/Context/ShopContext.jsx
+++ frontend/src/Context/ShopContext.jsx
@@ -1,13 +1,7 @@
-/**
- * This file sets up a context for managing the state of a shop application.
- * It uses React's Context API to provide a global state that can be accessed
- * by any component within the application.
-
- * @module ShopContext
- */
-
-import React, { createContext } from "react";
+import React, { createContext, useState } from "react";
 import all_product from "../Components/Assets/all_product";
+
+
 
 /**
  * ShopContext is a React context that will be used to share the shop's product data 
@@ -15,19 +9,36 @@ import all_product from "../Components/Assets/all_product";
  */
 export const ShopContext = createContext(null);
 
+
+const getDefaultCart = () => {
+    let cart = {};
+    for (let index = 0; index < all_product.length+1; index++) {
+        cart[index] = 0;
+    }
+    return cart;
+};
+
+
 /**
  * ShopContextProvider is a component that wraps its children with the ShopContext.Provider.
  * It provides the value of the context, which in this case includes the list of all products.
- *
- * @param {Object} props - The properties passed to the component, including children.
- * @returns {JSX.Element} - A component that provides the shop context to its children.
  */
+
 const ShopContextProvider = (props) => {
 
-    // The value that will be provided to any component subscribed to this context
-    const contextValue = {
-        all_product // This can be expanded to include more state or actions as needed
-    };
+    const [cartItems,setCartItems] = useState(getDefaultCart()); 
+   
+    const addToCart = (itemId)=>{
+        setCartItems((prev) => ({...prev,[itemId]:prev[itemId]+1}));
+        // console.log(cartItems);
+    }    
+   
+    const removeFromCart = (itemId)=>{
+        setCartItems((prev) => ({...prev,[itemId]:prev[itemId]-1}));
+    }  
+
+    const contextValue = {all_product,cartItems,addToCart,removeFromCart};
+
 
     return (
         <ShopContext.Provider value={contextValue}>
--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -2,15 +2,21 @@ import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
 import Breadcrumb from "../Components/Breadcrumbs/Breadcrumb";
 import { ShopContext } from "../Context/ShopContext";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
+import DescriptionBox from "../Components/DescriptionBox/DescriptionBox";
+import RelatedProducts from "../Components/RelatedProducts/RelatedProudcts";
 
 const Product = () => {
     const {all_product} = useContext(ShopContext);
     const {productId} = useParams();
     const product = all_product.find((e) => e.id === Number(productId));
-    
+
     return (
         <div>
             <Breadcrumb product={product}/>
+            <ProductDisplay product={product}/>
+            <DescriptionBox/>
+            <RelatedProducts/>
         </div>
     )
 }
