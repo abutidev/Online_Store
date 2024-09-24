4d73509de338f40d00c44431f2d5610fd4aa15d7
Tokelo Thulo <gelukthulo@gmail.com>
2024-09-24 14:18:27 +0200

    Web app fully compatibel with different devices

--- frontend/src/Pages/ShopCategory.jsx
+++ frontend/src/Pages/ShopCategory.jsx
@@ -8,14 +8,14 @@ const ShopCategory = (props) => {
     const {all_product} = useContext(ShopContext);
     return (
         <div className="shop-category">
-            <img src={props.banner} alt="" />
-            <div className="shopcategory-indexSort">
+            <img className='shopcategory-banner' src={props.banner} alt="" />
+            <div className='shopcategory-indexSort'>
                 <p><span>Showing 1-12</span> out of 36 products</p>
-            </div>
-            <div className="shopcategory-sort">
-                Sort by <img src={dropdown_icon} alt="" />
-            </div>
-            <div className="shopcategory-products">
+                <div className='shopcategory-sort'>
+                    Sort by <img src={dropdown_icon} alt="" />
+                </div>
+            </div>                
+            <div className='shopcategory-products'>
                 {all_product.map((item, i) => {
                     if (props.category === item.category) {
                         return <Item key={i} id={item.id} name={item.name}  image={item.image}
